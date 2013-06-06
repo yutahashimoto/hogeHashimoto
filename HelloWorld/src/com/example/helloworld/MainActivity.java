8717f029989aa1222349028640409c497bd05694
yutahashimoto <yuuta.hashimoto@nts.ricoh.co.jp>
2013-06-06 14:38:42 +0900

    commit

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toast.makeText(MainActivity.this, "h e l l o aaa", Toast.LENGTH_LONG).show();
+		Toast.makeText(MainActivity.this, "h e l l o ggg", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
