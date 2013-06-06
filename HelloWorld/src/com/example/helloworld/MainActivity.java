f2b2bf19888877764bc01a9c4917ec9f9748a408
yutahashimoto <yuuta.hashimoto@nts.ricoh.co.jp>
2013-06-06 09:57:15 +0900

    橋本のhello worldアプリ
    Change-Id: I17b0eaf02e5e79d415aa472215f62264b5f5cc89
    Signed-off-by: yutahashimoto <yuuta.hashimoto@nts.ricoh.co.jp>

---
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toast.makeText(MainActivity.this, "h e l l o", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
